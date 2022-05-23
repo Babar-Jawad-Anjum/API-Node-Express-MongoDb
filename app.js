593b6577ddf3d6c207e0ab3f3c44cf4442fa9275
Babar <babarjawad010@gmail.com>
2022-05-23 15:08:47 +0500

    cors commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import Models from './Models/models.js'
+import cors from 'cors'
+
 
 const app = express();
+app.use(cors());
 
 app.use(express.json()); 
 
